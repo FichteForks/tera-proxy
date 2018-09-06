295c633a396a0c33804255e3766422023dce43d9
Caali <caali-tera@outlook.com>
2018-09-07 00:40:11 +0200

    Improved launch script.

--- TeraProxy.bat
+++ TeraProxy.bat
@@ -1,9 +1,12 @@
+@echo off
 @setlocal enableextensions
 @cd /d "%~dp0/bin/lib"
 
 WHERE node > NUL 2> NUL
 IF %ERRORLEVEL% NEQ 0 (
-  ECHO Node.js is not installed. Please go to the #proxy channel of https://discord.gg/maqBmJV and follow the installation guide.
+  ECHO ERROR: Node.js is not installed!
+  ECHO ERROR: Please go to the #proxy channel of https://discord.gg/maqBmJV and follow the installation guide.
+  ECHO(
   PAUSE
 ) ELSE (
   START cmd.exe /k "node index.js"
